23d451244e9c649dbea652e34ff264a86b0b3ea8
MohammedHelal <mohd.omar.helal@gmail.com>
2025-05-10 16:13:48 +0800

    updating app.jsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,35 +8,13 @@ import {
   RouteObject,
 } from "react-router-dom";
 
-import Home from "./pages/home/Home";
+//import Home from "./pages/home/Home";
 import Products from "./pages/products/Products";
 import Checkout from "./pages/checkout/Checkout";
 import RootLayout from "./layout/RootLayout";
 import "./App.css";
 
 function App() {
-  /*const [initialMount, setInitialMount] = useState(true);
-
-  //redux store cart and dispatch
-  const cart = useAppSelector((store) => store.cart);
-  const dispatch = useAppDispatch();
-
-  //fetching cart info from local storage on first render
-  useEffect(() => {
-    dispatch(fetchCart());
-  });
-
-  
-  //sending data to local storage with any change to the cart
-  useEffect(() => {
-    //using initial mount state to not send data on initial mount/load
-    if (initialMount) {
-      setInitialMount(false);
-    } else {
-      localStorage.setItem("cart", JSON.stringify(cart));
-    }
-  }, [cart]);*/
-
   const paths: RouteObject[] = [
     {
       path: "/",
@@ -65,8 +43,3 @@ function App() {
 }
 
 export default App;
-
-/*
-
-        
-*/
